5ed9c1b9cd73b6a5392ae61b6ce4e48623c6c8d6
Cristian-Estevez <cuentacursosprogramacion@gmail.com>
2021-10-08 15:10:39 -0300

    Created add task functionality

--- frontend/static/frontend/scripts/script.js
+++ frontend/static/frontend/scripts/script.js
@@ -1,9 +1,26 @@
 buildList();
 
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+};
+const csrftoken = getCookie('csrftoken');
+
 function buildList(){
     var todoList = document.getElementById('todo-list');
     var url = 'http://127.0.0.1:8000/api/task-list/';
-
+    todoList.innerHTML = '';
     fetch(url)
     .then((resp) => resp.json())
     .then(function(data){
@@ -24,4 +41,23 @@ function buildList(){
             todoList.innerHTML += item;
         }
     });
-}
+};
+
+var form = document.getElementById('form-wrapper');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    var url = "http://127.0.0.1:8000/api/task-create/";
+    var title = document.getElementById('title').value;
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body:JSON.stringify({'title': title})
+    }
+    ).then(function(response){
+        buildList();
+        document.getElementById('form').reset();
+    })
+});
