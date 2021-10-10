1a0631adbd7890b48b25bb3bcb6089f34c6b3a87
Cristian-Estevez <cuentacursosprogramacion@gmail.com>
2021-10-10 11:40:52 -0300

    Create task in folder api ready

--- api/urls.py
+++ api/urls.py
@@ -6,9 +6,10 @@ app_name = 'api'
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
     path('folder-list/', views.folderList, name='folder-list'),
-    path('task-list/', views.taskList, name='task-list'),   
-    path('task-create/', views.taskCreate, name='task-create'),
-    path('task-update/<str:pk>/', views.taskUpdate, name='task-update'),
-    path('task-delete/<str:pk>/', views.taskDelete, name='task-delete'),
+    path('folder-create/', views.folderCreate, name='folder-create'),
+    path('folder-list/<str:pk>/task-list/', views.taskList, name='task-list'),   
+    path('folder-list/<str:pk>/task-create/', views.taskCreate, name='task-create'),
+    path('folder-list/task-update/<str:pk>/', views.taskUpdate, name='task-update'),
+    path('folder-list/task-delete/<str:pk>/', views.taskDelete, name='task-delete'),
 ]
 
--- api/views.py
+++ api/views.py
@@ -16,16 +16,15 @@ def apiOverview(request):
     return Response(api_urls)
 
 @api_view(['GET'])
-def taskList(request):
-
-    tasks = Task.objects.all().order_by('-id')
+def taskList(request, pk):
+    folder = Folder.objects.get(id=pk)
+    tasks = folder.task_set.all().order_by('-id')
     serializer = TaskSerializer(tasks, many=True)
     
     return Response(serializer.data)
 
 @api_view(['POST'])
-def taskCreate(request):
-
+def taskCreate(request, pk):
     serializer = TaskSerializer(data=request.data)
 
     if serializer.is_valid():
@@ -56,4 +55,14 @@ def folderList(request):
     folders = Folder.objects.all()
     serializer = FolderSerializer(folders, many=True)
 
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def folderCreate(request):
+
+    serializer = FolderSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    
     return Response(serializer.data)
